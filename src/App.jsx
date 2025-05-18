ea7599924345f75502bdfc5a389c7f91d15824ad
Adithya Shaji <adithyas038@gmail.com>
2025-05-18 20:16:41 +0530

    Added routing between the pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,19 @@ import Hero from './Components/Hero/Hero'
 import About from './Components/About/About'
 import Projects from './Components/Projects/Projects'
 import Contact from './Components/Contact/Contact'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 const App = () => {
+  const router = createBrowserRouter([
+    {path:"/",element:<><Navbar/><Hero/></>},
+    {path:"/about",element:<><Navbar/><About/></>},
+    {path:"/projects",element:<><Navbar/><Projects/></>},
+    {path:"/contact",element:<><Navbar/><Contact/></>}
+  ])
   return (
     <div>
-      <Navbar/>
-      <Hero/>
-      <About/>
-      <Projects/>
-      <Contact/>
+      
+      <RouterProvider router={router}/>
     </div>
   )
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import logo from '../../assets/logo.jpg'
+import { NavLink } from 'react-router-dom'
 const Navbar = () => {
   return (
     <div className='flex items-center justify-between m-4'>
       <img className='h-12' src={logo}/>
-      <ul className='flex items-center gap-8 text-sm cursor-pointer'>
-        <li>Home</li>
-        <li>About</li>
-        <li>Services</li>
-        <li>Portfolio</li>
-        <li>Contact</li>
+      <ul className='flex items-center gap-16 text-sm cursor-pointer'>
+        <NavLink className={(e)=>{return e.isActive?"text-purple-600":"text-white"}} to="/"><li>Home</li></NavLink>  
+        <NavLink className={(e)=>{return e.isActive?"text-purple-600":"text-white"}} to="/about"><li>About</li></NavLink>  
+        <NavLink className={(e)=>{return e.isActive?"text-purple-600":"text-white"}} to="/projects"><li>Project</li></NavLink>  
+        <NavLink className={(e)=>{return e.isActive?"text-purple-600":"text-white"}} to="/contact"><li>Contact</li></NavLink>  
       </ul>
       <div className='text-sm m- p-4 bg-gradient-to-r from-purple-500 to-pink-500 rounded-full hover:from-pink-400 hover:to-purple-400 cursor-pointer'>Connect With Me</div>
     </div>
